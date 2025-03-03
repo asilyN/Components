47ec14f92bd38916ea8596e6a9740454d0679b45
asilyN <150698908+asilyN@users.noreply.github.com.>
2025-03-03 21:52:29 +0800

    with finalize code and a running backend

--- server/supabaseclient.ts
+++ server/supabaseclient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-const SUPABASE_URL = "https://xlqqwswnnzgnusgyxnoh.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhscXF3c3dubnpnbnVzZ3l4bm9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5MjMwODQsImV4cCI6MjA1NjQ5OTA4NH0.ga2Wxe0qpE67N0_K7ilThcvpY76HNqVsTDXdM8HRt4A";
+dotenv.config();
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_KEY!);
